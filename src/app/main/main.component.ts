96160420ca2ceeb2a4e79a38e049014345687f66
David Weber <weberdavid91@gmail.com>
2024-03-16 13:12:40 +0100

    added config-form.component

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {WheelOfChoicesComponent} from "../wheel-of-choices/wheel-of-choices.component";
+import {ConfigFormComponent} from "../config-form/config-form.component";
 
 @Component({
   selector: 'app-main',
   standalone: true,
   imports: [
-    WheelOfChoicesComponent
+    WheelOfChoicesComponent,
+    ConfigFormComponent
   ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
